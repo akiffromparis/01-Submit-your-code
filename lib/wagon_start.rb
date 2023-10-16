ce09a5fc602839ec9a6c75e130d7eb8691709395
akiffromparis <148014954+akiffromparis@users.noreply.github.com>
2023-10-16 22:42:40 +0200

    Update wagon_start.rb

--- lib/wagon_start.rb
+++ lib/wagon_start.rb
@@ -1,3 +1,10 @@
-def wagon_start
-  return "That's how it starts" # TODO: change this code
+require "wagon_start"
+
+describe "#wagon_start" do
+
+  it "should return the exact message 'That's how it starts'" do
+    response = wagon_start
+    expect(response).to eq("That's how it starts")
+  end
+
 end
